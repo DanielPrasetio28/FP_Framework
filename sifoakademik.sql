5f7787b02dd00561467102b777d527ef1fcb270e
DanielPrasetio28 <danielprasetiobudiman@gmail.com>
2024-12-05 21:03:38 +0700

    Database

---
+++ sifoakademik.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2024 at 03:02 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sifoakademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id` int(11) NOT NULL,
+  `nama_kelas` varchar(50) NOT NULL,
+  `jurusan` enum('MIPA','IPS') NOT NULL,
+  `tingkat` enum('X','XI','XII') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id`, `nama_kelas`, `jurusan`, `tingkat`) VALUES
+(1, '1', 'MIPA', 'X'),
+(5, '1', 'IPS', 'XII'),
+(7, '4', 'MIPA', 'X'),
+(8, '2', 'IPS', 'XI'),
+(9, '3', 'MIPA', 'X'),
+(10, '3', 'IPS', 'X'),
+(11, '1', 'IPS', 'XII'),
+(19, '2', 'IPS', 'XII'),
+(20, '10', 'MIPA', 'XII');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nisn` varchar(20) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `angkatan` year(4) NOT NULL,
+  `kelas_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nisn`, `nama`, `angkatan`, `kelas_id`) VALUES
+('1122334456', 'Daniel Prasetio Budiman Raynegha', '2024', 20),
+('1122334459', 'Vazcha Tezza Lonica Raynegha', '2022', 9),
+('12345671', 'Gabriel Prasetio Budiman', '2024', 20),
+('1234567122', 'Eddy Budiman', '2022', 19);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nisn`),
+  ADD UNIQUE KEY `nisn` (`nisn`),
+  ADD KEY `kelas_id` (`kelas_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`kelas_id`) REFERENCES `kelas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
